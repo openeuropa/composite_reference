b04b0b47ff397c8d641233094509328d47fbb97c
upchuk <danny@webomelette.com>
2020-06-09 10:51:21 +0200

    OPENEUROPA-3135: Refactoring composite reference manager to check also for base fields.

--- src/CompositeReferenceFieldManager.php
+++ src/CompositeReferenceFieldManager.php
@@ -4,10 +4,13 @@ declare(strict_types = 1);
 
 namespace Drupal\composite_reference;
 
+use Drupal\Component\Utility\NestedArray;
+use Drupal\Core\Entity\EntityFieldManagerInterface;
 use Drupal\Core\Entity\EntityInterface;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Field\BaseFieldDefinition;
+use Drupal\Core\Field\FieldConfigInterface;
 use Drupal\Core\Field\FieldDefinitionInterface;
-use Drupal\field\FieldConfigInterface;
 
 /**
  * Manager class for composite reference fields.
@@ -22,13 +25,23 @@ class CompositeReferenceFieldManager implements CompositeReferenceFieldManagerIn
   protected $entityTypeManager;
 
   /**
+   * The entity field manager.
+   *
+   * @var \Drupal\Core\Entity\EntityFieldManagerInterface
+   */
+  protected $entityFieldManager;
+
+  /**
    * CompositeReferenceFieldManager constructor.
    *
    * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
    *   The entity type manager.
+   * @param \Drupal\Core\Entity\EntityFieldManagerInterface $entityFieldManager
+   *   The entity field manager.
    */
-  public function __construct(EntityTypeManagerInterface $entity_type_manager) {
+  public function __construct(EntityTypeManagerInterface $entity_type_manager, EntityFieldManagerInterface $entityFieldManager) {
     $this->entityTypeManager = $entity_type_manager;
+    $this->entityFieldManager = $entityFieldManager;
   }
 
   /**
@@ -36,35 +49,23 @@ class CompositeReferenceFieldManager implements CompositeReferenceFieldManagerIn
    */
   public function getReferencingEntities(EntityInterface $entity): array {
     $referencing_entities = [];
-    // Get all entity reference fields.
-    $field_config_storage = $this->entityTypeManager->getStorage('field_config');
-    $entity_reference_field_ids = $field_config_storage->getQuery()
-      ->condition('field_type', ['entity_reference', 'entity_reference_revisions'], 'IN')
-      ->execute();
-    $entity_reference_fields = $field_config_storage->loadMultiple($entity_reference_field_ids);
 
-    // Only use fields that reference the given entity's type.
-    $fields_referencing_entity_type = [];
-    /** @var \Drupal\field\FieldConfigInterface $field */
-    foreach ($entity_reference_fields as $field) {
-      $field_settings = $field->getSettings();
-      if ($field_settings['handler'] === 'default:' . $entity->getEntityTypeId()) {
-        $fields_referencing_entity_type[$field->getTargetEntityTypeId()][] = $field->getName();
-      }
-    }
+    $reference_fields = $this->getReferenceFields($entity->getEntityTypeId());
 
     // Load all entities that have a reference to the given entity.
-    foreach ($fields_referencing_entity_type as $field_entity_type_id => $field_names) {
-      $entity_type_storage = $this->entityTypeManager->getStorage($field_entity_type_id);
+    foreach ($reference_fields as $entity_type => $field_names) {
+      $entity_type_storage = $this->entityTypeManager->getStorage($entity_type);
       $query = $entity_type_storage->getQuery('OR');
       foreach ($field_names as $field_name) {
-        $query->condition($field_name, $entity->id());
+        $query->condition("$field_name.target_id", $entity->id());
       }
+
       $ids = $query->execute();
       if ($ids) {
         $referencing_entities = array_merge($referencing_entities, $entity_type_storage->loadMultiple($ids));
       }
     }
+
     return $referencing_entities;
   }
 
@@ -72,7 +73,7 @@ class CompositeReferenceFieldManager implements CompositeReferenceFieldManagerIn
    * {@inheritdoc}
    */
   public function onDelete(EntityInterface $entity, FieldDefinitionInterface $field_definition): void {
-    if ($field_definition instanceof FieldConfigInterface && $field_definition->getThirdPartySetting('composite_reference', 'composite', FALSE)) {
+    if ($this->assertFieldDefinition($field_definition)) {
       $referenced_entities = $entity->get($field_definition->getName())->referencedEntities();
       /** @var \Drupal\Core\Entity\EntityInterface $referenced_entity */
       foreach ($referenced_entities as $referenced_entity) {
@@ -81,6 +82,56 @@ class CompositeReferenceFieldManager implements CompositeReferenceFieldManagerIn
         }
       }
     }
+  }
+
+  /**
+   * Get the fields that reference entities of this type.
+   *
+   * @param string $entity_type
+   *   The entity type.
+   *
+   * @return array
+   *   An array of field names, keyed by the entity type they reference from.
+   */
+  protected function getReferenceFields(string $entity_type): array {
+    $entity_reference_map = $this->entityFieldManager->getFieldMapByFieldType('entity_reference');
+    $entity_reference_revisions_map = $this->entityFieldManager->getFieldMapByFieldType('entity_reference_revisions');
+    $map = NestedArray::mergeDeep($entity_reference_map, $entity_reference_revisions_map);
+    $reference_fields = [];
+    foreach ($map as $entity_type => $fields) {
+      $definitions = array_filter($this->entityFieldManager->getFieldStorageDefinitions($entity_type), function ($definition, $field_name) use ($fields, $entity_type) {
+        // Include only the fields types from our map and that reference the
+        // entity type of the passed entity.
+        return in_array($field_name, array_keys($fields)) && $definition->getSetting('target_type') === $entity_type;
+      }, ARRAY_FILTER_USE_BOTH);
+
+      $reference_fields[$entity_type] = array_keys($definitions);
+    }
+
+    return array_filter($reference_fields);
+  }
+
+  /**
+   * Asserts that the field definition is a composite reference.
+   *
+   * @param \Drupal\Core\Field\FieldDefinitionInterface $field_definition
+   *   The field definition.
+   *
+   * @return bool
+   *   TRUE if it's composite, FALSE otherwise.
+   */
+  protected function assertFieldDefinition(FieldDefinitionInterface $field_definition): bool {
+    if ($field_definition instanceof FieldConfigInterface) {
+      // This works for both configurable fields, as well as base field
+      // overrides.
+      return $field_definition->getThirdPartySetting('composite_reference', 'composite', FALSE);
+    }
+
+    if ($field_definition instanceof BaseFieldDefinition) {
+      return (bool) $field_definition->getSetting('composite_reference');
+    }
+
+    return FALSE;
   }
 
 }
